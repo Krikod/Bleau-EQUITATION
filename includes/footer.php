bd2c53c1cc9ab55a856bd13c612c2bd5e8d212c8
Krikod <kristen.kod@gmail.com>
2017-03-01 15:22:43 +0100

    ajout des fichiers de base après récupération

---
+++ includes/footer.php
@@ -0,0 +1,68 @@
+<!-- Footer incluant:
+- onglet contact
+- logos partenaires
+- partager
+- copyrights -->
+
+
+
+<?php include "includes/contact.php;" ?>
+
+
+	</body>
+</html>
+
+
+
+<!-- Exemple Bootstrap dans l'atelier Space_invador
+
+
+
+<div class="container container_custom">
+
+
+	<div class="row row_custom">
+		<div class="col-md-offset-3 col-md-1 full"></div>
+		<div class="col-md-offset-4 col-md-1 full"></div> <!--avec xs on conserve le vert
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-4 col-md-1 full"></div>
+		<div class="col-md-offset-2 col-md-1 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-3 col-md-6 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-2 col-md-2 full"></div>
+		<div class="col-md-offset-1 col-md-2 full"></div>
+		<div class="col-md-offset-1 col-md-2 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-1 col-md-10 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-1 col-md-1 full"></div>
+		<div class="col-md-offset-1 col-md-6 full"></div>
+		<div class="col-md-offset-1 col-md-1 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-1 col-md-1 full"></div>
+		<div class="col-md-offset-1 col-md-1 full"></div>
+		<div class="col-md-offset-4 col-md-1 full"></div>
+		<div class="col-md-offset-1 col-md-1 full"></div>
+	</div>
+	<div class="row row_custom">
+		<div class="col-md-offset-4 col-md-2 height">
+			<div class="col-md-8 full">
+			<!-- tenter de diviser une div pour alonger pattes, dans la classe .height
+			</div>
+		</div>
+		<div class="col-md-offset-0 col-md-2 height">
+			<div class="col-md-offset-4 col-md-8 full"></div>
+	</div>
+
+	<!-- Latest compiled and minified JavaScript
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+
+-->
---
+++ pratiquer.php
@@ -0,0 +1,3 @@
+<?php include "includes/contact.php;" ?>
+<?php include "includes/footer.php"; ?>
+<?php include "includes/nav.php"; ?>
