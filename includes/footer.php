81299767b2bde3d4f95a54cce6c3aedb0cf1d662
Krikod <kristen.kod@gmail.com>
2017-03-01 18:19:53 +0100

    1ère ébauche footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,16 +1,48 @@
-<!-- Footer incluant:
-- onglet contact
-- logos partenaires
-- partager
-- copyrights 
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+	<title>footer</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style_footer.css">
+</head>
+<body>
 
-TEST BRANCHE FOOTER
-TEST 2-->
 
 
+<!-- Partager - contact - partenaires -->
 
-<?php include "includes/contact.php;" ?>
 
+<footer>
+  <div class="container">
+    <div class="row">
+      
+      <div class="col-md-4 col-sm-6 footerleft ">
+        <div class="share">Partager</div>
+        <p>--> INSERER RESEAUX SOCIAUX.</p>
+      </div>
+      
+      <div class="col-md-2 col-sm-6 paddingtop-bottom">
+        <h6 class="contact">CONTACT</h6>
+      </div>
+      
+      <div class="col-md-3 col-sm-6 paddingtop-bottom">
+        <h6 class="partners">Partenaires</h6>
+      </div>
+
+    </div>
+  </div>
+
+<!--Copyright-->
+
+<div class="copyright">
+  <div class="container">
+    <div class="col-xs-6 col-md-6">
+      <p>© Copyrights WCS - 2017</p>
+    </div>
+  </div>
+</div>
+</footer>
 
 	</body>
 </html>
