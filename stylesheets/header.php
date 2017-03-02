63f154ec7f5a73bae6da6d61d5d18263c0531780
KyoLuma <couchotcynthia99@gmail.com>
2017-03-02 16:28:05 +0100

    add header

---
+++ stylesheets/header.php
@@ -0,0 +1,62 @@
+<!---------------- HTML du Header ------------------>
+
+<!-- <!DOCTYPE html>
+<html lang="fr">
+<head>
+	<meta charset="UTF-8">
+	<title>Bleau'Equitation</title>
+
+	 Call CSS 
+
+	<link rel="stylesheet" type="text/css" href="style_header.css">
+
+	Bootstrap
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+	Font 
+
+	<link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet"> 
+
+	
+
+</head>
+<body> -->
+	
+	<div class="container-fluid container_custom">
+		
+			
+			<div class="row row_custom">
+			<img src="http://image.noelshack.com/fichiers/2017/09/1488362072-image.png" class="col-xs-offset-0 col-xs-4 bleauequitation">
+			<img src="http://image.noelshack.com/fichiers/2017/09/1488465893-kiher.png" class="col-xs-offset-0 col-xs-2 horse_logo">
+			<div class="col-xs-offset-3 col-xs-3 intro">
+				<div class="intro_close">Introduction</div>
+				<div class="col-xs-offset-0 col-xs-12 intro_open">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+			</div></div>
+		
+		    	<div class="row row_custom">
+			
+			
+				<a href="https://www.facebook.com/"><img src="https://images.seeklogo.net/2016/09/facebook-icon-preview.png" class="col-xs-offset-0 col-xs-1 logo_facebook"/></a>
+				
+				<a href="https://twitter.com/?lang=fr"><img src="https://cdn3.iconfinder.com/data/icons/social-icons-5/607/Twitterbird.png" class="col-xs-offset-0 col-xs-1 logo_twitter"/></a>
+
+				</div>
+
+<!--
+
+		</div>
+
+		
+
+	</div>
+
+	
+
+</body>
+</html> -->
