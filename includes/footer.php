51e0ce7b6b5b74aefff255ccc7e217c6b3435207
Krikod <kristen.kod@gmail.com>
2017-03-02 17:40:25 +0100

    footer: ajout code php et css

--- includes/footer.php
+++ includes/footer.php
@@ -1,103 +1,111 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
-	<title>footer</title>
-	<meta charset="utf-8">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
-	<link rel="stylesheet" type="text/css" href="./assets/css/style_footer.css">
+    <title>footer</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="./assets/css/style_footer.css">
 </head>
+
 <body>
 
 
-
-<!-- Partager - contact - partenaires -->
-
-
 <footer>
-  <div class="container">
-    <div class="row">
-      
-      <div class="col-md-4 col-sm-6 footerleft ">
-        <div class="share">Partager</div>
-        <p>--> INSERER RESEAUX SOCIAUX.</p>
-      </div>
-      
-      <div class="col-md-2 col-sm-6 paddingtop-bottom">
-        <h6 class="contact">CONTACT</h6>
-      </div>
-      
-      <div class="col-md-3 col-sm-6 paddingtop-bottom">
-        <h6 class="partners">Partenaires</h6>
-      </div>
+    <div class="container-fluid">
+        <div class="row text text-center">
+            <div class="col-md-4 col-sm-12">
+                <h4>Partager</h4>
+                <ul class="social-network social-circle">
+                    <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a>
+                    </li>
+                    <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a>
+                    </li>
+                </ul>
+            </div>
 
+            <div class="col-md-4 col-sm-12">
+                <!-- Button to trigger modal -->
+                <a href="#myModal" role="button" class="btn btn-custom" data-toggle="modal">Contact</a>
+                <!-- Modal -->
+                <div id="myModal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                                <h4 class="modal-title">Ecrivez-nous !</h4>
+                            </div>
+                            <div class="modal-body">
+
+                                <!-- Formulaire de contact-->
+                                <form class="form-horizontal" name="commentform" method="post" action="send_form_email.php">
+                                    <div class="form-group">
+                                        <label class="control-label col-md-4" for="first_name">Prénom</label>
+                                        <div class="col-md-6">
+                                            <input type="text" class="form-control" id="first_name" name="first_name" placeholder="First Name" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-4" for="last_name">Nom</label>
+                                        <div class="col-md-6">
+                                            <input type="text" class="form-control" id="last_name" name="last_name" placeholder="Last Name" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-4" for="email">Email</label>
+                                        <div class="col-md-6 input-group">
+                                            <span class="input-group-addon">@</span>
+                                            <input type="email" class="form-control" id="email" name="email" placeholder="Email Address" />
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label class="control-label col-md-4" for="comment">Votre message</label>
+                                        <div class="col-md-6">
+                                            <textarea rows="6" class="form-control" id="comments" name="comments" placeholder="Your question or comment here"></textarea>
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <div class="col-md-6">
+                                            <button type="submit" value="Submit" class="btn btn-custom pull-right">Envoyer</button>
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-4 col-sm-12 hidden-xs">
+                <h4>Partenaires</h4>
+
+                <ul class="partners">
+                    <li><img src="http://www.grandparquet.com/wp-content/uploads/2016/05/logo.png" alt="Logo du Grand Parquet" />
+                    </li>
+                    <li><img src="https://image.jimcdn.com/app/cms/image/transf/dimension=162x10000:format=png/path/sb17ab31817e946c7/image/id8e005e33c48a861/version/1453064990/image.png" alt="Logo de l'hippodrome de la Solle" />
+                    </li>
+                    <li><img src="http://www.shn-fontainebleau.com/media/images/logo.png" alt="Logo du C" />
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div class="row text-center">
+            <div class="col-md-12 col-sm-12">
+                <h3>&#169; Wild Code School - 2017</h3>
+            </div>
+        </div>
     </div>
-  </div>
-
-<!--Copyright-->
-
-<div class="copyright">
-  <div class="container">
-    <div class="col-xs-6 col-md-6">
-      <p>© Copyrights WCS - 2017</p>
-    </div>
-  </div>
-</div>
 </footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 
-	</body>
+</body>
 </html>
-
-
-
-<!-- Exemple Bootstrap dans l'atelier Space_invador
-
-
-
-<div class="container container_custom">
-
-
-	<div class="row row_custom">
-		<div class="col-md-offset-3 col-md-1 full"></div>
-		<div class="col-md-offset-4 col-md-1 full"></div> <!--avec xs on conserve le vert
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-4 col-md-1 full"></div>
-		<div class="col-md-offset-2 col-md-1 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-3 col-md-6 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-2 col-md-2 full"></div>
-		<div class="col-md-offset-1 col-md-2 full"></div>
-		<div class="col-md-offset-1 col-md-2 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-1 col-md-10 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-1 col-md-1 full"></div>
-		<div class="col-md-offset-1 col-md-6 full"></div>
-		<div class="col-md-offset-1 col-md-1 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-1 col-md-1 full"></div>
-		<div class="col-md-offset-1 col-md-1 full"></div>
-		<div class="col-md-offset-4 col-md-1 full"></div>
-		<div class="col-md-offset-1 col-md-1 full"></div>
-	</div>
-	<div class="row row_custom">
-		<div class="col-md-offset-4 col-md-2 height">
-			<div class="col-md-8 full">
-			<!-- tenter de diviser une div pour alonger pattes, dans la classe .height
-			</div>
-		</div>
-		<div class="col-md-offset-0 col-md-2 height">
-			<div class="col-md-offset-4 col-md-8 full"></div>
-	</div>
-
-	<!-- Latest compiled and minified JavaScript
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-
-
--->
