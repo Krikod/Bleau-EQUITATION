29ef2ae116251e6decf21a585339573bfc11a80f
Florian Grandjean <florian.pdf@gmail.com>
2017-03-03 15:15:21 +0100

    clean

--- indexe.php
+++
@@ -1,6 +0,0 @@
-<?php include "includes/header.php"; ?>
-<?php include "includes/main.php"; ?>
-<?php include "includes/footer.php"; ?>
-<?php include "includes/contact.php;" ?>
-
-
--- pratiquer.php
+++ pratiquer.php
@@ -3,245 +3,193 @@
 
 	<meta charset="UTF-8">
 
-
 	<title>Pratiquer</title>
 
-<head>
+  <head>
 
-			<!-- Bootstrap -->
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  	<!-- Bootstrap -->
+  	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
-				<!-- call css -->
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
 
-			<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-			<link rel="stylesheet" type="text/css" href="assets/orstyle.css">
+    <!-- Google Font -->
+    <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
 
-			 	<!-- Optional theme -->
+		<!-- call css -->
+		<link rel="stylesheet" type="text/css" href="assets/orstyle.css">
 
-			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
-			 <link href="https://fonts.googleapis.com/css?family=Cinzel" rel="stylesheet">
-</head>
+  </head>
 
-<body>
+  <body>
 
-	<?php include "includes/header.php;" ?>
+  	<?php include "includes/header.php;" ?>
 
-	<h1>Pratiquer à Fontainebleau</h1>
+  	<h1>Pratiquer à Fontainebleau</h1>
+  	<!-- presentation à pratiquer à Fontainebleau -->
 
-										<!-- presentation à pratiquer à Fontainebleau -->
-
-<div class="container container_intro">
-
-
+    <div class="container container_intro">
       <div class="row">
-
-
         <section class="col-lg-12">
-
-         <div> Monter à cheval ou à poney est un plaisir qui permet de se ressourcer… <br />
-          Outre la pratique sportive qui contribue au bien-être physique des pratiquants et au développement psycho- moteur chez l’enfant, <br />l’équitation est un sport de couple.<br />La relation qui se créée entre le cavalier et son cheval ou son poney lui apporte une complicité inédite avec un animal<br /> et lui fait acquérir de nombreuses connaissances sur le milieu naturel.</div>
-
-
+          <p>
+            Monter à cheval ou à poney est un plaisir qui permet de se ressourcer… <br />
+            Outre la pratique sportive qui contribue au bien-être physique des pratiquants et au développement psycho- moteur chez l’enfant, <br />l’équitation est un sport de couple.<br />La relation qui se créée entre le cavalier et son cheval ou son poney lui apporte une complicité inédite avec un animal<br /> et lui fait acquérir de nombreuses connaissances sur le milieu naturel.
+          </p>
         </section>
-
-
       </div>
 
-										<!-- video de presentation -->
-
-	<div class="container container video">
-
-        
-
-<iframe width="560" height="315" src="https://www.youtube.com/embed/dJhpnvrCj1A?showinfo=0&amp;start=8" frameborder="0" allowfullscreen></iframe>
-
- 		
-
-
-
- 		</div>
-
-
-
-										<!--  tableau présentation centre -->
-
-<h2> Centre Equestre </h2>
-
-<section class="col-sm-">
-
-
-        </section>
-
+      <!-- video de presentation -->
+      <div class="container container video">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/dJhpnvrCj1A?showinfo=0&amp;start=8" frameborder="0" allowfullscreen></iframe>
       </div>
 
       <div class="row">
 
+        <!--  tableau présentation centre -->
+        <h2> Centre Equestre </h2>
         <section class="col-sm-offset-2 col-sm-8">
-
           <table class="table table-inverse"> 
-
             <thead>
-
               <tr>
-
                 <th>Centre</th>
-
                 <th>Contact</th>
-
               </tr>
-
             </thead>
-
             <tbody>
 
               <tr>
-
-                <td><h3>Caval et go à Fontainebleau<h3/></td>
-
-                <td>3 rue Paul Tavernier 77300 Fontainebleau Tél : 09 80 32 90 42 
-                <a>www.cavalngo.com</a>
-</td>
-
+                <td>
+                  <h3>Caval et go à Fontainebleau<h3/>
+                </td>
+                <td>
+                  <p>3 rue Paul Tavernier 77300 Fontainebleau Tél : 09 80 32 90 42 </p>
+                  <a>www.cavalngo.com</a>
+                </td>
               </tr>
 
               <tr>
-
-                <td><h3>Ecuries Bellifontaines <h3/></td>
-
-                <td><div>La faisanderie Route de l'ermitage <br />77300 Fontainebleau <br />Tél : 01 64 22 63 49 
-                <a>www.poneyclubdefontainebleau.ffe.com </div></a> 
-</td>
-
+                <td>
+                  <h3>Ecuries Bellifontaines <h3/>
+                </td>
+                <td>
+                  <p>
+                    La faisanderie Route de l'ermitage <br/ >
+                    77300 Fontainebleau <br />
+                    Tél : 01 64 22 63 49 
+                    <a>www.poneyclubdefontainebleau.ffe.com</a> 
+                  </p>
+                </td>
               </tr>
 
               <tr>
-
-                <td><h3>SHN<h3/></td>
-
-                <td><div>Quartier du carrousel av Maintenon, 77300 Fontainebleau
-					centres équestres, équitation<br />
-					Prestations : Dressage,  Saut d'obstacle,  Pension pour chevaux<br />
-					<a>http://www.shn-fontainebleau.com/</a></div>
-</td>
-
+                <td>
+                  <h3>SHN<h3/>
+                </td>
+                <td>
+                  <p>
+                    Quartier du carrousel av Maintenon, 77300 Fontainebleau centres équestres, équitation<br />
+                    Prestations : Dressage,  Saut d'obstacle,  Pension pour chevaux<br />
+                    <a>http://www.shn-fontainebleau.com/</a>
+                  </p>
+                </td>
               </tr>
 
               <tr>
-
-                <td><h3>Ferrier Claude</h3></td>
-
-                <td><div>3 all Palais, 77300 Fontainebleau <br />centres équestres, équitation<br />
-					<a>http://www.kelest.fr/a/ferrier-claude-fontainebleau</div></a>
-</td>
-
+                <td>
+                  <h3>Ferrier Claude</h3>
+                </td>
+                <td>
+                  <p>
+                    3 all Palais, 77300 Fontainebleau <br />
+                    centres équestres, équitation<br />
+                    <a>http://www.kelest.fr/a/ferrier-claude-fontainebleau</a>
+                  </p>
+                </td>
               </tr>
-
-             
 
             </tbody>
-
           </table>
-
         </section>
-
       </div>
-
-      </div>
-
     </div>
 
-  										<!-- Promenade à cheval -->  
+  	<!-- Promenade à cheval -->  
+    <h2> Promenade à cheval </h2>
+    <div class="row">
+      <section class="col-sm-offset-2 col-sm-8">
+        <table class="table">
+          <thead>
 
-  <h2> Promenade à cheval </h2>
+            <tr>
+              <th>Centre</th>
+              <th>Contact</th>
+            </tr>
 
-<section class="col-sm-">
+          </thead>
+          <tbody>
 
-        </section>
+            <tr>
+              <td>
+                <h3>Caval et go à Fontainebleau</h3>
+              </td>
+              <td>
+                <p>
+                  3 rue Paul Tavernier 77300 Fontainebleau<br /> France Tél : 09 80 32 90 42 
+                  <a>www.cavalngo.com</a>
+                </p>
+              </td>
+            </tr>
 
-      </div>
+            <tr>
+              <td>
+                <h3>Horse dreams à Achères la forêt</h3>
+              </td>
+              <td>
+                <p>
+                  30 bis rue du Château 77760 Achère la forêt<br />
+                  Tél. 06 70 46 03 69<a>http://www.horse-dreams.com</a>
+                </p>
+              </td>
+            </tr>
 
-      <div class="row">
+            <tr>
+              <td>
+                <h3>Centre équestre BPAL à Bois-le-Roi</h3>
+              </td>
+              <td>
+                <p>
+                  Rue de Tournezy, 77590 Bois le roi <br />
+                  Tél : 01 64 81 33 00 <a>http://bois-le-roi.iledeloisirs.fr</a>
+                </p>
+              </td>
+            </tr>
 
-        <section class="col-sm-offset-2 col-sm-8">
+            <tr>
+              <td>
+                <h3>Ecuries Bellifontaines à Fontainebleau</h3>
+              </td>
+              <td>
+                <p>
+                  La Faisanderie (derrière l'Insead) <br />
+                  Route de l'Ermitage 
+                  77300 FONTAINEBLEAU <br /> 
+                  Tél : 01 64 22 63 49
+                  <a>https://poneyclubdefontainebleau.ffe.com/</a>
+                </p>
+              </td>
+            </tr>
 
-          <table class="table">
-
-            <thead>
-
-              <tr>
-
-                <th>Centre</th>
-
-                <th>Contact</th>
-
-              </tr>
-
-            </thead>
-
-            <tbody>
-
-              <tr>
-
-                <td><h3>Caval et go à Fontainebleau</h3></td>
-
-                <td><div>3 rue Paul Tavernier 77300 Fontainebleau<br /> France Tél : 09 80 32 90 42 
-					<a>www.cavalngo.com</a></div>
-</td>
-
-              </tr>
-
-              <tr>
-
-                <td><h3>Horse dreams à Achères la forêt</h3></td>
-
-                <td><div>30 bis rue du Château 77760 Achère la forêt<br />
-					Tél. 06 70 46 03 69<a>http://www.horse-dreams.com</a></div>
-
-</td>
-
-              </tr>
-
-              <tr>
-
-                <td><h3>Centre équestre BPAL à Bois-le-Roi</h3></td>
-
-                <td><div>Rue de Tournezy, 77590 Bois le roi <br />
-					Tél : 01 64 81 33 00 <a>http://bois-le-roi.iledeloisirs.fr</a>
-
-</td>
-
-              </tr>
-
-              <tr>
-
-                <td><h3>Ecuries Bellifontaines à Fontainebleau</h3></td>
-
-                <td><div>La Faisanderie (derrière l'Insead) <br />Route de l'Ermitage 
-					77300 FONTAINEBLEAU <br /> Tél : 01 64 22 63 49
-					<a>https://poneyclubdefontainebleau.ffe.com/</a></div>
-</td>
-
-              </tr>
-
-             
-
-            </tbody>
-
-          </table>
-
-        </section>
-
-      </div>
-
-      </div>
-
+          </tbody>
+        </table>
+      </section>
     </div>
-				<?php include "includes/nav.php"; ?>
-				<?php include "includes/footer.php"; ?>
 
-	
-</body>
+    <?php include "includes/nav.php"; ?>
+    <?php include "includes/footer.php"; ?>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+  </body>
 </html>
 
-<!-- Latest compiled and minified JavaScript -->
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
