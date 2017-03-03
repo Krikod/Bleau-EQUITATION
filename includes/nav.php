8767041285fc2d1aef2a7d94c6de751d0ec71eff
Krikod <kristen.kod@gmail.com>
2017-03-03 15:13:56 +0100

    navigation stade initial

--- includes/nav.php
+++ includes/nav.php
@@ -1,1 +1,42 @@
 <!-- Navigation à reprendre sur les pages sauf index -->
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <title>Navigation</title>
+    <meta charset="utf-8">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Crimson+Text" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="../assets/css/style_nav.css">
+</head>
+<body>
+
+
+
+<div class="container-fluid">
+    <div class="row text text-center">
+        <div class="col-md-12 col-sm-12">
+            <ul class="nav">
+                <li><a href="#" class="accueil" title="Accueil">Accueil</a>
+                </li>
+                <li><a href="#" class="icoPratiquer" title="Pratiquer">Pratiquer</a>
+                </li>
+                <li><a href="#" class="icoVoir" title="Voir"></i>Voir</a>
+                </li>
+                <li><a href="#" class="icoActus" title="Actualités">Actus</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+</body>
+</html>
